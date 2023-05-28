2060040c2a10c16bb7a81a2b4b3a2f51651841ac
Jongseo Lee <james0614ll@gmail.com>
2023-05-28 14:51:59 +0000

    add toggle button & change readme

--- perlinnoise.js
+++ perlinnoise.js
@@ -80,6 +80,7 @@ function setResult(x, y) {
 function draw() {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
+  let colorValue = 0;
 
   for (let n = 0; n < PN_gridNum; n++) {
     for (let m = 0; m < PN_gridNum; m++) {
@@ -91,7 +92,28 @@ function draw() {
             ][i] +
               1) *
             128; //0~256
-          let colorValue = getColorValue(rawValue);
+          let modifiedValue = rawValue / 256;
+          if (modifiedValue > 0.85) {
+            ctx.fillStyle = "#ffffff"; // snow
+          } else if (modifiedValue < 0.85 && modifiedValue > 0.485) {
+            colorValue = getColorValue((-modifiedValue + 0.9) * 256);
+            ctx.fillStyle = "#00" + colorValue + "00";
+          } else if (modifiedValue <= 0.485 && modifiedValue > 0.45) {
+            ctx.fillStyle = "#fff5bb"; // beach sand
+          } else if (modifiedValue <= 0.45) {
+            colorValue = getColorValue((modifiedValue + 0.5) * 256);
+            ctx.fillStyle = "#0000" + colorValue;
+          }
+          ctx.fillRect(
+            m * PN_cellLength * PN_gridLength + i * PN_cellLength,
+            n * PN_cellLength * PN_gridLength + j * PN_cellLength,
+            PN_cellLength,
+            PN_cellLength
+          );
+
+          // Black & White
+          /*
+          colorValue = getColorValue(rawValue);
           ctx.fillStyle = "#" + colorValue + colorValue + colorValue;
           ctx.fillRect(
             m * PN_cellLength * PN_gridLength + i * PN_cellLength,
@@ -99,6 +121,7 @@ function draw() {
             PN_cellLength,
             PN_cellLength
           );
+          */
         }
       }
     }
